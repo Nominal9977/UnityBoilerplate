d0f114947efc3f4506f5621327c091b0e771f631
Kele Fireheart <kele.fireheart@mymail.champlain.edu>
2025-10-21 20:37:44 -0400

    Started Pathfinding script and made new scene

---
+++ Assets/Scripts/Pathfinding/Pathfinding.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Pathfinding : MonoBehaviour
+{
+    
+    private List<Vector3> frontier = new List<Vector3>();
+    private List<Vector3> cameFrom = new List<Vector3>();
+    
+    public List<Vector3> world = new List<Vector3>();
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+}
