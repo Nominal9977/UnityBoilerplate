f6b608a37bbff2a69cab0dd4bcd1e8ee18632f5d
Noah Freedley <42617616+KiritoBro2004@users.noreply.github.com>
2025-10-27 22:08:22 -0400

    updates

--- Assets/Scripts/Path Smoothing/PathSmoothing.cs
+++ Assets/Scripts/Path Smoothing/PathSmoothing.cs
@@ -5,17 +5,37 @@ using UnityEngine;
 public class Smoother
 {
     // Constructor With Offset Point
-    public Smoother(Vector3 start_point, Vector3 offset_point, Vector3 end_point) {
-        this.start_point = start_point;
-        this.offset_point = offset_point;
-        this.end_point = end_point;
+    public Smoother(MapGeneration map_gen, Vector3 start_point, Vector3 offset_point, Vector3 end_point) {
+        this.start_point = new Vector3(
+            start_point.x * (map_gen.cellPadding + map_gen.cellSize),
+            0,
+            start_point.z * (map_gen.cellPadding + map_gen.cellSize)
+        );
+        this.offset_point = new Vector3(
+            offset_point.x * (map_gen.cellPadding + map_gen.cellSize),
+            0,
+            offset_point.z * (map_gen.cellPadding + map_gen.cellSize)
+        );
+        this.end_point = new Vector3(
+            end_point.x * (map_gen.cellPadding + map_gen.cellSize),
+            0,
+            end_point.z * (map_gen.cellPadding + map_gen.cellSize)
+        );
         has_offset_point = true;
     }
 
     // Constructor Without Offset Point
-    public Smoother(Vector3 start_point, Vector3 end_point) {
-        this.start_point = start_point;
-        this.end_point = end_point;
+    public Smoother(MapGeneration map_gen, Vector3 start_point, Vector3 end_point) {
+        this.start_point = new Vector3(
+            start_point.x * (map_gen.cellPadding + map_gen.cellSize),
+            0,
+            start_point.z * (map_gen.cellPadding + map_gen.cellSize)
+        );
+        this.end_point = new Vector3(
+            end_point.x * (map_gen.cellPadding + map_gen.cellSize),
+            0,
+            end_point.z * (map_gen.cellPadding + map_gen.cellSize)
+        );
         has_offset_point = false;
     }
 
@@ -27,6 +47,7 @@ public class Smoother
     // Changing Variables
     public float current_time = 0.0f;
     public Vector3 current_position;
+    public string type = "";
 
     bool has_offset_point = false;
     public bool finished = false;
@@ -79,6 +100,7 @@ public class PathSmoothing : MonoBehaviour
     int smoother_index = 0;
     int last_exit = (int)Directions.South;
 
+    [SerializeField] MapGeneration map_gen;
     public void SetPath(List<Node> path) {
         // Update Path
         this.node_list = path;
@@ -100,7 +122,7 @@ public class PathSmoothing : MonoBehaviour
         exit_directions.Add(last_exit);
 
         // Calculate Direction For Each Point In Path
-        for (int i = 0; i < node_list.Count - 1; i++) {
+        for (int i = 0; i < node_list.Count - 2; i++) {
             Vector3 current_node_position = node_list[i].position;
             Vector3 next_node_position = node_list[i+1].position;
             Vector3 far_node_position = node_list[i+2].position;
@@ -108,27 +130,80 @@ public class PathSmoothing : MonoBehaviour
             Vector3 difference_a = next_node_position - current_node_position;
             Vector3 difference_b = far_node_position - next_node_position;
 
+            // Check If Starting Diagonal, Otherwise Turn Normally
+            int exit_direction_a = DifferenceToNormalDirectionEnum(difference_a);
+            int exit_direction_b = DifferenceToNormalDirectionEnum(difference_b);
+
             if (last_exit <= (int)Directions.East) // Normal Direction
             {
                 if(difference_a == difference_b)
                 {
                     // Check If Continue Straight
-                    Smoother next_path = new Smoother(current_node_position - DirectionEnumToDirection(last_exit), next_node_position - difference_a/2);
+                    int opposite_last_exit = DirectionEnumToOppositeDirectionEnum(last_exit);
+                    Smoother next_path = new Smoother(map_gen, current_node_position - DirectionEnumToDirection(opposite_last_exit)/2, next_node_position - difference_a/2);
                     final_path.Add(next_path);
+                    next_path.type = "Normal Straight";
                 } else 
                 {
-                    // Check If Starting Diagonal, Otherwise Turn Normally
-                    int exit_direction_a = DifferenceToNormalDirectionEnum(difference_a);
-                    int exit_direction_b = DifferenceToNormalDirectionEnum(difference_b);
+                    // Check If Starting A Diagonal, Otherwise Turn Normally
+                    bool went_diagonal = false;
 
                     // We Know We Are Turning, So We Check If We Are Staying Oriented
                     if(exit_direction_b == last_exit)
                     {
+                        // Check If Opposite To Exit Is A "Wall"
+                        int opposite_last_exit = DirectionEnumToOppositeDirectionEnum((int)last_exit);
+                        Vector3 opposite_last_exit_direction = DirectionEnumToDirection(opposite_last_exit);
+                        Vector2 oppposite_last_exit_index = new Vector2(current_node_position.x + opposite_last_exit_direction.x, current_node_position.z + opposite_last_exit_direction.z);
+                        
+                        bool wall_exists = MapGeneration.IsPositionBlocked(oppposite_last_exit_index);
 
+                        if(!wall_exists)
+                        {
+                            Vector3 start_point = current_node_position - DirectionEnumToDirection(opposite_last_exit)/2;
+                            Vector3 offset_point = current_node_position - (difference_a / 4) + (opposite_last_exit_direction / 4);
+                            Vector3 end_point = current_node_position + (difference_a / 2) + (opposite_last_exit_direction / 2);
+                            Smoother next_path = new Smoother(map_gen, start_point, offset_point, end_point);
+                            next_path.type = "Starting Diagonal (Type A)";
+                            final_path.Add(next_path);
+                            went_diagonal = true;
+                            i += 1;
+                            last_exit = NormalDirectionEnumTo45CounterClockwiseEnum(last_exit, "Start Diagonal");
+                        }
+                    } else if(NormalDirectionEnumTo90CounterClockwiseEnum(exit_direction_b) == last_exit)
+                    {
+                        // Check If Opposite To Exit Is A "Wall"
+                        int opposite_last_exit = DirectionEnumToOppositeDirectionEnum(exit_direction_b);
+                        Vector3 opposite_last_exit_direction = DirectionEnumToDirection(opposite_last_exit);
+                        Vector2 oppposite_last_exit_index = new Vector2(current_node_position.x + opposite_last_exit_direction.x, current_node_position.z + opposite_last_exit_direction.z);
+
+                        bool wall_exists = MapGeneration.IsPositionBlocked(oppposite_last_exit_index);
+
+                        if (!wall_exists)
+                        {
+                            Vector3 start_point = current_node_position - DirectionEnumToDirection(opposite_last_exit) / 2;
+                            Vector3 offset_point = current_node_position - (difference_a / 4) + (opposite_last_exit_direction / 4);
+                            Vector3 end_point = current_node_position + (difference_a / 2) + (opposite_last_exit_direction / 2);
+                            Smoother next_path = new Smoother(map_gen, start_point, offset_point, end_point);
+                            next_path.type = "Starting Diagonal (Type B)";
+                            final_path.Add(next_path);
+                            went_diagonal = true;
+                            i += 1;
+                            last_exit = NormalDirectionEnumTo45ClockwiseEnum(last_exit);
+                        }
                     }
 
-                    // see if next exit direction is the opposite (right if left, left if right)
-
+                    // No Other Option Besides Normal Turn
+                    if(went_diagonal == false)
+                    {
+                        int opposite_last_exit = DirectionEnumToOppositeDirectionEnum((int)last_exit);
+                        Vector3 start_point = current_node_position - DirectionEnumToDirection(opposite_last_exit)/2;
+                        Vector3 end_point = current_node_position + (difference_a / 2);
+                        Smoother next_path = new Smoother(map_gen, start_point, current_node_position, end_point);
+                        next_path.type = "Turning";
+                        final_path.Add(next_path);
+                        last_exit = exit_direction_a;
+                    }
                 }
 
                 // cases
@@ -137,9 +212,70 @@ public class PathSmoothing : MonoBehaviour
                 // -shift to diagonal left or right
             } else // Diagonal Direction
             {
-                // cases
-                // - continue diagonal
-                // - fall out left or right (back to normal direction)
+                int left_direction = NormalDirectionEnumTo45CounterClockwiseEnum(last_exit, "Continue Diagonal");
+                int right_direction = NormalDirectionEnumTo45ClockwiseEnum(last_exit);
+                bool staying_diagonal = false;
+
+                if (exit_direction_a == left_direction)
+                {
+                    if(exit_direction_b == right_direction)
+                    {
+                        // Check If Opposite To Exit Is A "Wall"
+                        int opposite_last_exit = DirectionEnumToOppositeDirectionEnum((int)last_exit);
+                        Vector3 exit_b_direction = DirectionEnumToDirection(exit_direction_b);
+                        Vector3 opposite_last_exit_direction = DirectionEnumToDirection(opposite_last_exit);
+                        Vector2 oppposite_last_exit_index = new Vector2(current_node_position.x + exit_b_direction.x, current_node_position.z + exit_b_direction.z);
+
+                        bool wall_exists = MapGeneration.IsPositionBlocked(oppposite_last_exit_index);
+
+                        if (!wall_exists)
+                        {
+                            Vector3 start_point = current_node_position - DirectionEnumToDirection(opposite_last_exit) / 2;
+                            Vector3 end_point = current_node_position + (difference_a / 2) + (opposite_last_exit_direction / 2);
+                            Smoother next_path = new Smoother(map_gen, start_point, end_point);
+                            next_path.type = "Continue Diagonal A";
+                            final_path.Add(next_path);
+                            staying_diagonal = true;
+                            i += 1;
+                        }
+                    }
+                }
+
+
+                if (exit_direction_a == right_direction)
+                {
+                    if (exit_direction_b == left_direction)
+                    {
+                        // Check If Opposite To Exit Is A "Wall"
+                        int opposite_last_exit = DirectionEnumToOppositeDirectionEnum((int)last_exit);
+                        Vector3 exit_b_direction = DirectionEnumToDirection(exit_direction_b);
+                        Vector3 opposite_last_exit_direction = DirectionEnumToDirection(opposite_last_exit);
+                        Vector2 oppposite_last_exit_index = new Vector2(current_node_position.x + exit_b_direction.x, current_node_position.y + exit_b_direction.y);
+
+                        bool wall_exists = MapGeneration.IsPositionBlocked(oppposite_last_exit_index);
+
+                        if (!wall_exists)
+                        {
+                            Vector3 start_point = current_node_position - DirectionEnumToDirection(opposite_last_exit) / 2;
+                            Vector3 end_point = current_node_position + (difference_a / 2) + (opposite_last_exit_direction / 2);
+                            Smoother next_path = new Smoother(map_gen, start_point, end_point);
+                            next_path.type = "Continue Diagonal B";
+                            final_path.Add(next_path);
+                            staying_diagonal = true;
+                            i += 1;
+                        }
+                    }
+                }
+
+                if(staying_diagonal == false)
+                {
+                    Vector3 start_point = current_node_position - DirectionEnumToDirection(last_exit) / 2;
+                    Vector3 end_point = next_node_position - difference_a / 2;
+                    Smoother next_path = new Smoother(map_gen, start_point, end_point);
+                    next_path.type = "End Diagonal";
+                    final_path.Add(next_path);
+                    last_exit = exit_direction_a;
+                }
             }
         }
 
@@ -166,7 +302,7 @@ public class PathSmoothing : MonoBehaviour
             case (int)Directions.South_West:
                 return new Vector3(-1, 0, -1);
             default:
-                Debug.LogError("SHOULD NEVER REACH");
+                Debug.LogError("SHOULD NEVER REACH - DirectionEnumToDirection");
                 break;
         }
         return new Vector3();
@@ -190,6 +326,79 @@ public class PathSmoothing : MonoBehaviour
             return (int)Directions.South;
         }
     } 
+
+    public int NormalDirectionEnumTo90CounterClockwiseEnum(int direction)
+    {
+        switch (direction)
+        {
+            case (int)Directions.North:
+                return (int)Directions.West;
+            case (int)Directions.East:
+                return (int)Directions.North;
+            case (int)Directions.South:
+                return (int)Directions.East;
+            case (int)Directions.West:
+                return (int)Directions.South;
+            default:
+                Debug.LogError("SHOULD NEVER REACH - NormalDirectionEnumTo90CounterClockwiseEnum  - INPUT: " + direction);
+                break;
+        }
+        return (int)Directions.East;
+    }
+
+    public int NormalDirectionEnumTo45CounterClockwiseEnum(int direction, string from)
+    {
+        switch (direction)
+        {
+            case (int)Directions.North:
+                return (int)Directions.North_West;
+            case (int)Directions.East:
+                return (int)Directions.North_East;
+            case (int)Directions.South:
+                return (int)Directions.South_East;
+            case (int)Directions.West:
+                return (int)Directions.South_West;
+            case (int)Directions.North_West:
+                return (int)Directions.West;
+            case (int)Directions.North_East:
+                return (int)Directions.North;
+            case (int)Directions.South_East:
+                return (int)Directions.East;
+            case (int)Directions.South_West:
+                return (int)Directions.South;
+            default:
+                Debug.LogError("SHOULD NEVER REACH - NormalDirectionEnumTo45CounterClockwiseEnum - INPUT: " + direction + " CALLED BY: " + from);
+                break;
+        }
+        return (int)Directions.East;
+    }
+
+    public int NormalDirectionEnumTo45ClockwiseEnum(int direction)
+    {
+        switch (direction)
+        {
+            case (int)Directions.North:
+                return (int)Directions.North_East;
+            case (int)Directions.East:
+                return (int)Directions.South_East;
+            case (int)Directions.South:
+                return (int)Directions.South_West;
+            case (int)Directions.West:
+                return (int)Directions.North_West;
+            case (int)Directions.North_West:
+                return (int)Directions.North;
+            case (int)Directions.North_East:
+                return (int)Directions.East;
+            case (int)Directions.South_East:
+                return (int)Directions.South;
+            case (int)Directions.South_West:
+                return (int)Directions.West;
+            default:
+                Debug.LogError("SHOULD NEVER REACH - NormalDirectionEnumTo90CounterClockwiseEnum");
+                break;
+        }
+        return (int)Directions.East;
+    }
 
     public int DirectionEnumToOppositeDirectionEnum(int direction)
     {
@@ -241,11 +450,11 @@ public class PathSmoothing : MonoBehaviour
             node_list.Add(new_node);
 
             // Pick A Random Of 4 Directions (x and z)
-            Vector3 next_direction = directions[Random.Range(0, directions.Count)];
-            while(next_direction == -last_direction)
-            {
-                next_direction = directions[Random.Range(0, directions.Count)];
-            }
+            Vector3 next_direction = new Vector3(2, 0, 0);// directions[Random.Range(0, directions.Count)];
+            //while(next_direction == -last_direction)
+            //{
+              //  next_direction = directions[Random.Range(0, directions.Count)];
+            //}
 
             // Add To New Position
             current_position += next_direction;
@@ -258,13 +467,21 @@ public class PathSmoothing : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (final_path.Count == 0 || smoother_index >= final_path.Count)
+        {
+            print("RETURNING UPDATE EARLY");
+            return;
+        }
+
         // Get Current Smoother
         Smoother current_smoother = final_path[smoother_index];
 
-        current_smoother.Update(Time.deltaTime);
+        current_smoother.Update(Time.deltaTime/1.5f);
         if(current_smoother.finished)
         {
             smoother_index += 1;
+            Smoother next = final_path[smoother_index];
+            print(next.type + "S: " + next.start_point + " E: " + next.end_point);
         }
 
         transform.position = current_smoother.current_position;
